7c4cbb5642ae33c4fb968b55e18234e9711d81d5
Ali Ahmad <ali.ahmed@tintash.com>
2022-11-23 16:55:32 +0500

    appspecfile updated

--- scripts/BeforeInstallHook.sh
+++ scripts/BeforeInstallHook.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
 yum update -y
-sudo yum install node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install --lts
+node -e "console.log('Running Node.js ' + process.version)"
 sudo yum install npm
