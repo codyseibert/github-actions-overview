eaa459f5be9f0013e44b60cc3ce8c1deab64b196
Ali Ahmad <ali.ahmed@tintash.com>
2022-11-23 17:24:10 +0500

    appspecfile updated

--- scripts/BeforeInstallHook.sh
+++ scripts/BeforeInstallHook.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 set -e
 yum update -y
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install 16
-node -e "console.log('Running Node.js ' + process.version)"
