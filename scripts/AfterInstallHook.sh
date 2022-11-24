2610e2dedca7c0617481571a804d70d155533a78
Ali Ahmad <ali.ahmed@tintash.com>
2022-11-24 17:12:17 +0500

    appspecfile updated

--- scripts/AfterInstallHook.sh
+++ scripts/AfterInstallHook.sh
@@ -5,8 +5,10 @@ touch ~/.bash_profile
 cat $HOME
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install --lts
+nvm install 16
 node -e "console.log('Running Node.js ' + process.version)"
 cd /
 npm install
 npm run start
+
+
