1043f11d615e821e0137debb3d3c9aae72e510b2
Ali Ahmad <ali.ahmed@tintash.com>
2022-11-24 14:46:17 +0500

    appspecfile updated

--- scripts/AfterInstallHook.sh
+++ scripts/AfterInstallHook.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-set -e
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install --lts
+node -e "console.log('Running Node.js ' + process.version)"
 cd /
 npm install
 npm run start
